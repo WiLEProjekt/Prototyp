946659681e00833f6febe4cb992d1418abd5969d
Dennis Rieke <dennis.rieke.93@gmail.com>
2018-05-18 14:36:28 +0200

    bla

--- Tool2/TraceGenerator/mainwindow.cpp
+++ Tool2/TraceGenerator/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,9 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_btnSave_clicked()
+{
+    std::cout << "blub" << std::endl;
 }
