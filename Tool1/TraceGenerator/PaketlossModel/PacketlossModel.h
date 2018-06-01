cbbd47832a9488871d49e4f55fd64a87c2a7574d
dominic <d_lani01@uni-muenster.de>
2018-06-01 18:56:39 +0200

    fixed random number generator documentation

--- Tool1/TraceGenerator/PaketlossModel/PacketlossModel.h
+++ Tool1/TraceGenerator/PaketlossModel/PacketlossModel.h
@@ -9,7 +9,7 @@ using namespace std;
 class PacketlossModel {
 protected:
     mt19937 generator; //Mersenne Twister Engine
-    uniform_real_distribution<float> distribution; //Uniform floats in [a,b) => (0.1, 1.0) is not 100% correct. Error is neglectable
+    uniform_real_distribution<float> distribution; //Uniform floats in [a,b) => a=0.0, b=1.0 is not 100% correct. Correct would be [0.0, 1.0]. Now its [0.0, 1.0). Error is neglectable
     long numPackets;
     float generateRandomNumber();
     PacketlossModel(unsigned int seed, long numPackets):numPackets(numPackets){generator.seed(seed); distribution = *new uniform_real_distribution<float>(0.0, 1.0);}
