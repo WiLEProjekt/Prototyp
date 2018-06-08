5da079e74693582e909e6d5b2da596c0d2d8b8fe
dlanie <d_lani01@uni-muenster.de>
2018-06-08 16:00:23 +0200

    Updated R-Script for ECDF and Kolmogorov-Smirnov Test

--- Tool1/Validationtool/R-Scripts/GilbertElliotPlots.R
+++ Tool1/Validationtool/R-Scripts/GilbertElliotPlots.R
@@ -1,4 +1,12 @@
-GilbertElliotNetem = read.table("Documents/Prototyp/Tool1/Validationtool/build/GilbertElliotVal.txt")
-y = GilbertElliotNetem[,1]
-x = 1:65771
-plot(x,y, type="l", xlab="Burstnumber", ylab="Burstsize [number of packets]", main="Gilbert-Elliot Netem p=20%, r=70% 1-k=5% 1-h=95%")
+GilbertElliotNetem = read.table("Dokumente/Prototyp/Tool1/Validationtool/build/GilbertElliotNetem.txt")
+GIlbertElliotTool = read.table("Dokumente/Prototyp/Tool1/Validationtool/build/GilbertElliotTool.txt")
+yGEN = GilbertElliotNetem[,1]
+yGET = GIlbertElliotTool[,1]
+FnGEN <- ecdf(yGEN)
+FnGET <- ecdf(yGET)
+x <- c(seq(-10,60,by=10))
+plot(FnGEN, lty=2, lwd=3, verticals=TRUE, do.points=FALSE, col="red", xlim=c(-10,60), main="ECDF of Gilbert-Elliot with p=20%, r=70%, 1-k=5%, 1-h=95%", xlab="Burstsize [Number of Packets] (Negative = Loss, Positive = No Loss)", ylab="Cumulative Probability")
+plot(FnGET, lty=3, lwd=3, verticals=TRUE, do.points=FALSE, add=TRUE, col="blue")
+legend("bottomright", lwd=2, lty=c(2,3), col=c("red","blue"), legend=c("Netem","Our Tool"), bty="n")
+abline(v=0, lty=3)
+ks.test(yGEN, yGET, alternative="two.sided")
