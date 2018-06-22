fa49de01ea568f439f8968ef496512fb10fdd6b7
dlanie <d_lani01@uni-muenster.de>
2018-06-22 13:07:15 +0200

    Merged with GE Param Extraction

--- Tool1/TraceGenerator/PacketLossToParameterParser.cpp
+++ Tool1/TraceGenerator/PacketLossToParameterParser.cpp
@@ -111,6 +111,7 @@ vector<bool> PacketLossToParameterParser::readFile(string filename) {
     while (file.get(c)) {
         trace.push_back(c != '0');
     }
+    trace.pop_back();
     return trace;
 }
 
@@ -276,30 +277,157 @@ float *PacketLossToParameterParser::parseSimpleGilbert(vector<bool> trace) {
 float *PacketLossToParameterParser::parseGilbertElliot(vector<bool> trace, unsigned int gMin) {
     vector<int> lossindices;
     vector<int> gapindices;
-    for (int i = 0; i < trace.size(); i++) { //find all loss indices
-        if (!trace[i]) {
-            cout << i << endl;
+    for(int i = 0; i<trace.size(); i++){ //find all loss indices
+        if(!trace[i]){
             lossindices.push_back(i);
         }
     }
-
-    for (int i = 1; i < lossindices.size(); i++) { //push
-        if (lossindices[i] - lossindices[i - 1] - 1 < 16) {
-            if (!(find(gapindices.begin(), gapindices.end(), lossindices[i - 1]) != gapindices.end())) {
-                gapindices.push_back(lossindices[i - 1]);
+//cout << lossindices.size() << endl;
+    for(int i = 1; i<lossindices.size(); i++){ //push
+        if(lossindices[i]-lossindices[i-1]-1 < 16){
+            if(!(find(gapindices.begin(), gapindices.end(), lossindices[i-1])!=gapindices.end())){
+                gapindices.push_back(lossindices[i-1]);
             }
-            if (!(find(gapindices.begin(), gapindices.end(), lossindices[i]) != gapindices.end())) {
+            if(!(find(gapindices.begin(), gapindices.end(), lossindices[i])!=gapindices.end())){
                 gapindices.push_back(lossindices[i]);
             }
         }
     }
 
-    //while()
-    cout << "gapindices" << endl;
-    for (int i = 0; i < gapindices.size(); i++) {
-        cout << gapindices[i] << endl;
+    vector<int> bursts; //startindex and endindex
+    bool start=false;
+    for(int i = 0; i<gapindices.size(); i++){
+        if(i==0){
+            bursts.push_back(gapindices[i]);
+        }else{
+            if((gapindices[i]-gapindices[i-1])-1 >=16){
+                bursts.push_back(gapindices[i-1]);
+                bursts.push_back(gapindices[i]);
+            }
+            if(i == gapindices.size()-1){
+                bursts.push_back(gapindices[i]);
+            }
+        }
     }
-    return nullptr;
+    if((trace.size()-1) - (bursts[bursts.size()-1])<16){
+        bursts[bursts.size()-1] = trace.size()-1;
+    }
+
+    int transingoodstate=0;
+    int transinbadstate=0;
+    if(bursts.size() == 0){
+        transingoodstate = trace.size()-1;
+    }else{
+        for(int i = 0; i<bursts.size();i++){
+            if(i%2 == 0){//even = start index of burst state found
+                if(i == 0){
+                    transingoodstate = transingoodstate + bursts[i];
+                }else{
+                    transingoodstate = transingoodstate + (bursts[i]-bursts[i-1]-1);
+                }
+            }else{ //odd = end index of burst state
+                transinbadstate = transinbadstate + (bursts[i]-bursts[i-1]+1);
+            }
+        }
+        if(bursts[bursts.size()-1]<trace.size()-1){
+            transingoodstate = transingoodstate + (trace.size()-1-bursts[bursts.size()-1]-1);
+        }
+    }
+
+    int goodBadStateTrans = 0;
+    int badGoodStateTrans = 0;
+    if(bursts[0] != 0 && bursts[bursts.size()-1] != trace.size()-1){
+        goodBadStateTrans = bursts.size()/2;
+        badGoodStateTrans = bursts.size()/2;
+    }else if(bursts[0] == 0 && bursts[bursts.size()-1] != trace.size()-1){
+        goodBadStateTrans = bursts.size()/2-1;
+        badGoodStateTrans = bursts.size()/2;
+    }else if(bursts[0] != 0 && bursts[bursts.size()-1] == trace.size()-1) {
+        goodBadStateTrans = bursts.size()/2;
+        badGoodStateTrans = bursts.size()/2-1;
+    }else{
+        goodBadStateTrans = bursts.size()/2-1;
+        badGoodStateTrans = bursts.size()/2-1;
+    }
+    //cout << "goodbad: " << goodBadStateTrans << endl;
+    //cout << "badgood: " << badGoodStateTrans << endl;
+
+    //cout << "tgood:" << transingoodstate << endl;
+    //cout << "tbad: " << transinbadstate << endl;
+
+    float p = (float)goodBadStateTrans/(float)transingoodstate;
+    float r = (float)badGoodStateTrans/(float)transinbadstate;
+    //cout << "p: " << p << endl;
+    //cout << "r: " << r << endl;
+    //BIS HIER GEHTS
+
+    //calculate k, h
+    int rcvinGoodState=0;
+    int lstinGoodState=0;
+    int rcvinBadState=0;
+    int lstinBadState=0;
+    for(int i = 0; i<bursts.size();i++){
+        if(i%2==0){
+            if(i==0){
+                for(int a = 0; a<bursts[i];a++){
+                    if(trace[a]==1){
+                        rcvinGoodState++;
+                    }else{
+                        lstinGoodState++;
+                    }
+                }
+                //cout << rcvinGoodState << endl;
+            }else{
+                for(int a = bursts[i-1]+1; a<bursts[i];a++){
+                    if(trace[a]==1){
+                        rcvinGoodState++;
+                    }else{
+                        lstinGoodState++;
+                    }
+                }
+            }
+
+        }else{
+            for(int a = bursts[i-1]; a<=bursts[i];a++){
+                if(trace[a]==1){
+                    rcvinBadState++;
+                }else{
+                    lstinBadState++;
+                }
+            }
+            //cout << "lst: " << lstinBadState << endl;
+        }
+    }
+    if(bursts[bursts.size()-1]<trace.size()-1){
+        for(int a = bursts[bursts.size()-1]+1; a<trace.size();a++){
+            if(trace[a]==1){
+                rcvinGoodState++;
+            }else{
+                lstinGoodState++;
+            }
+        }
+    }
+    float k = (float)rcvinGoodState/(float)(rcvinGoodState+lstinGoodState);
+    float h = (float)rcvinBadState/(float)(rcvinBadState+lstinBadState);
+    //cout << "rcvGood: " << rcvinGoodState << endl;
+    //cout << "lstGood: " << lstinGoodState << endl;
+    //cout << "rcvBad: " << rcvinBadState << endl;
+    //cout << "lstBad: " << lstinBadState << endl;
+    //cout << "k: " << k << endl;
+    //cout << "h: " << h << endl;
+
+/*
+    cout << "gapindices" << endl;
+    for(int i = 0; i<gapindices.size();i++){
+        cout << gapindices[i] << endl;
+    }*/
+
+/*
+    cout << "startend" << endl;
+    for(int i = 0; i<bursts.size(); i++){
+        cout << bursts[i] << endl;
+    }*/
+    return new float[4]{p, r, k, h};
 }
 
 float *PacketLossToParameterParser::parseBernoulli(vector<bool> trace) {
