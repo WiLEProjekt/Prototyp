200ca4401fa602a886c9514d2c92eb49af4575f1
Alexander Ditt <Aditt@uos.de>
2018-08-20 17:53:48 +0200

    Add files via upload

---
+++ Messtool/install_measurement_libs.sh
@@ -0,0 +1,21 @@
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt-get install build-essential 
+
+sudo apt install netperf
+
+sudo apt-get install openssh-server^C
+
+sudo apt-get install libpcap0.8-dev  
+
+sudo apt-get install wireshark
+
+sudo apt-get install git-core
+
+sudo apt install net-tools
+
+sudo apt install nmap
+
+sudo apt install valgrind
