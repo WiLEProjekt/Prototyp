676f927851b3ee64dc3fd69c482a9ff5a9dee1e4
Dominic <dlaniewski@uos.de>
2018-09-18 18:18:45 +0200

    fixed thread handling and system calls

--- Dominic_Tests/GesamtTool/client.py
+++ Dominic_Tests/GesamtTool/client.py
@@ -3,21 +3,20 @@ import threading
 from threading import Thread
 import subprocess
 import time
+import os
 
 def bandwidtUpload(): #open iperf server process for upload measurement
-    sub1 = subprocess.Popen(['iperf', '-c 127.0.0.1', '-p 5001'], stdout=subprocess.PIPE)
-    upOutput = sub1.communicate()[0].decode()
-    print(upOutput)
+    sub1 = subprocess.Popen("iperf -c 127.0.0.1 -p 5001 > cupload.txt", shell=True)
 
 def bandwidthDownload(): #open iperf server process for download measurement
-    sub2 = subprocess.Popen(['iperf', '-c 127.0.0.1', '-p 5002', '--reverse'], stdout=subprocess.PIPE)
-    downOutput = sub2.communicate()[0].decode()
-    print(downOutput)
+    sub2 = subprocess.Popen("iperf -c 127.0.0.1 -p 5002 --reverse > cdownload.txt", shell=True)
+
 
 if __name__ == "__main__":
     destIP = "127.0.0.1"
     destPort = 5000
     beginMessage = "Start"
+    stopMessage = "Stop"
 
     ####
     #socket handling
@@ -26,6 +25,7 @@ if __name__ == "__main__":
     tcpsock.connect((destIP, destPort))
     tcpsock.send(beginMessage.encode())
     time.sleep(2) # to assure that the server opened both iperf servers
+
     threads = []
     killevent = threading.Event()
     t1 = Thread(target=bandwidtUpload, args=())
@@ -36,3 +36,5 @@ if __name__ == "__main__":
     t2.start()
     for thread in threads:  # Wait till all threads are finished
         thread.join()
+    tcpsock.send(stopMessage.encode())
+    tcpsock.close()
--- Dominic_Tests/GesamtTool/server.py
+++ Dominic_Tests/GesamtTool/server.py
@@ -2,16 +2,22 @@ import socket
 import threading
 from threading import Thread
 import subprocess
+from subprocess import check_output
+import time
+import os
 
 def bandwidtUpload(): #open iperf server process for upload measurement
     sub1 = subprocess.Popen(['iperf', '-s', '-p 5001'], stdout=subprocess.PIPE)
     upOutput = sub1.communicate()[0].decode()
-    print(upOutput)
 
 def bandwidthDownload(): #open iperf server process for download measurement
     sub2 = subprocess.Popen(['iperf', '-s', '-p 5002', '--reverse'], stdout=subprocess.PIPE)
     downOutput = sub2.communicate()[0].decode()
-    print(downOutput)
+
+def tcpreceive(connection, address):
+    data = connection.recv(2048).decode()
+    if data == "Stop":
+        os.system("pkill iperf")
 
 if __name__ == "__main__":
     myIP = "127.0.0.1"
@@ -23,19 +29,25 @@ if __name__ == "__main__":
     ####
     tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
     tcpsock.bind((myIP, 5000))
-    tcpsock.listen()
-    conn, addrtcp = tcpsock.accept()
-    datatcp = conn.recv(2048).decode()
-    if datatcp == "Start":
-        threads = []
-        killevent = threading.Event()
-        t1 = Thread(target=bandwidtUpload, args=())
-        t2 = Thread(target=bandwidthDownload, args=())
-        threads.append(t1)
-        threads.append(t2)
-        t1.start()
-        t2.start()
-        for thread in threads: #Wait till all threads are finished
-            thread.join()
-    else:
-        print("Received wrong message, waiting for new connection!")
+    while True:
+        tcpsock.listen()
+        conn, addrtcp = tcpsock.accept()
+        datatcp = conn.recv(2048).decode()
+        if datatcp == "Start":
+            threads = []
+            killevent = threading.Event()
+            t1 = Thread(target=bandwidtUpload, args=())
+            t2 = Thread(target=bandwidthDownload, args=())
+            t3 = Thread(target=tcpreceive, args=(conn, addrtcp))
+            threads.append(t1)
+            threads.append(t2)
+            threads.append(t3)
+            t1.start()
+            t2.start()
+            t3.start()
+            for thread in threads: #Wait till all threads are finished
+                thread.join()
+            conn.close()
+            print("Waiting for new connection")
+        else:
+            print("Waiting for new connection")
