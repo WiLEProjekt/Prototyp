af6e6eded70a1ff5ec65a3d3ce28e435f9d5fab3
Dominic <dlaniewski@uos.de>
2018-09-19 16:49:16 +0200

    bandwidth measurement should work

--- Dominic_Tests/GesamtTool/client.py
+++ Dominic_Tests/GesamtTool/client.py
@@ -1,54 +1,62 @@
+import os
 import socket
 import threading
 from threading import Thread
-import subprocess
-import time
-import os
 
-def bandwidtUpload(): #open iperf server process for upload measurement
-    sub1 = subprocess.Popen("iperf -c 127.0.0.1 -p 5001 > cupload.txt", shell=True)
+def download():
+    os.system("iperf -c 127.0.0.1 -p 5001 --reverse > download.txt")
 
-def bandwidthDownload(): #open iperf server process for download measurement
-    sub2 = subprocess.Popen("iperf -c 127.0.0.1 -p 5002 --reverse > cdownload.txt", shell=True)
+def upload():
+    os.system("iperf -c 127.0.0.1 -p 5002 > upload.txt")
 
-def readMeasurement(filename):
-    file = open(filename,"r")
-    lines = file.readlines()
-    file.close()
-    print(lines)
-    lastLine = lines[-1]
+def readFile(filename):
+    file = open(filename, "r")
+    fileLines = file.readlines()
+    lastLine = fileLines[-1]
     return(lastLine)
 
+def calcBandwidthResult(lastFileLine):
+    print(lastFileLine)
+    speed = float(lastFileLine[-16:-10]) #fetch result and convert it to a float
+    entity = lastFileLine[-10:-1] #fetch entity in string format
+    if entity == "Gbits/sec":
+        value = speed*1000
+    elif entity == "Kbits/sec":
+        value = speed/1000
+    elif entity == "Mbits/sec":
+        value = speed
+    elif entity == "Tbits/sec":
+        value = speed*1000000
+    else:
+        value = "No valid measurement result!"
+    return(value)
 
 if __name__ == "__main__":
-    destIP = "127.0.0.1"
-    destPort = 5000
-    beginMessage = "Start"
-    stopMessage = "Stop"
-
-    ####################################################################################################################
-    #socket handling
-    ####################################################################################################################
-    tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
-    tcpsock.connect((destIP, destPort))
-    tcpsock.send(beginMessage.encode())
-    time.sleep(2) # to assure that the server opened both iperf servers
-
     threads = []
-    killevent = threading.Event()
-    t1 = Thread(target=bandwidtUpload, args=())
-    t2 = Thread(target=bandwidthDownload, args=())
+    t1 = Thread(target=download, args=())
+    t2 = Thread(target=upload, args=())
     threads.append(t1)
     threads.append(t2)
     t1.start()
     t2.start()
     for thread in threads:  # Wait till all threads are finished
         thread.join()
-    tcpsock.send(stopMessage.encode())
-    tcpsock.close()
-    #readMeasurement("cupload.txt")
-    #print(upload)
-    file = open("cupload", "r")
-    lines = file.readlines()
-    print(lines)
 
+    destIP = "127.0.0.1"
+    destPort = 5000
+    beginMessage = "Start"
+    stopMessage = "Stop"
+
+    ####################################################################################################################
+    # socket handling
+    ####################################################################################################################
+    tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
+    tcpsock.connect((destIP, destPort))
+    tcpsock.send(stopMessage.encode())
+    print("hier")
+    uploadLine = readFile("upload.txt")
+    downloadLine = readFile("download.txt")
+    uploadspeed = calcBandwidthResult(uploadLine)
+    downloadspeed = calcBandwidthResult(downloadLine)
+    print("Upload: {} Mbit/s".format(uploadspeed))
+    print("Download: {} Mbit/s".format(downloadspeed))
--- Dominic_Tests/GesamtTool/server.py
+++ Dominic_Tests/GesamtTool/server.py
@@ -1,53 +1,35 @@
+import os
 import socket
 import threading
 from threading import Thread
-import subprocess
-from subprocess import check_output
-import time
-import os
 
-def bandwidtUpload(): #open iperf server process for upload measurement
-    sub1 = subprocess.Popen(['iperf', '-s', '-p 5001'], stdout=subprocess.PIPE)
-    upOutput = sub1.communicate()[0].decode()
+def iperfServerReverse(): #closes automatically
+    os.system("iperf -s -p 5001 --reverse")
 
-def bandwidthDownload(): #open iperf server process for download measurement
-    sub2 = subprocess.Popen(['iperf', '-s', '-p 5002', '--reverse'], stdout=subprocess.PIPE)
-    downOutput = sub2.communicate()[0].decode()
+def iperfServer(): #does not close automatically -> client sends stop message when measurement is finished and other thread kills iperf so this thread finishes execution
+    os.system("iperf -s -p 5002")
 
-def tcpreceive(connection, address): #2. argument needed cuz of python specialities
-    data = connection.recv(2048).decode()
-    if data == "Stop":
+def tcpreceive():
+    myIP = "127.0.0.1"
+    tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
+    tcpsock.bind((myIP, 5000))
+    tcpsock.listen()
+    conn, addrtcp = tcpsock.accept()
+    datatcp = conn.recv(2048).decode()
+    if datatcp == "Stop":
         os.system("pkill iperf")
 
 if __name__ == "__main__":
-    myIP = "127.0.0.1"
-    destPort = "5000"
-    beginMessage = "Start"
-
-    ####
-    #socket handling
-    ####
-    tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
-    tcpsock.bind((myIP, 5000))
-    while True:
-        tcpsock.listen()
-        conn, addrtcp = tcpsock.accept()
-        datatcp = conn.recv(2048).decode()
-        if datatcp == "Start":
-            threads = []
-            killevent = threading.Event()
-            t1 = Thread(target=bandwidtUpload, args=())
-            t2 = Thread(target=bandwidthDownload, args=())
-            t3 = Thread(target=tcpreceive, args=(conn, addrtcp))
-            threads.append(t1)
-            threads.append(t2)
-            threads.append(t3)
-            t1.start()
-            t2.start()
-            t3.start()
-            for thread in threads: #Wait till all threads are finished
-                thread.join()
-            conn.close()
-            print("Waiting for new connection")
-        else:
-            print("Waiting for new connection")
+    threads = []
+    t1 = Thread(target=iperfServerReverse, args=())
+    t2 = Thread(target=iperfServer, args=())
+    t3 = Thread(target=tcpreceive, args=())
+    threads.append(t1)
+    threads.append(t2)
+    threads.append(t3)
+    t1.start()
+    t2.start()
+    t3.start()
+    for thread in threads:  # Wait till all threads are finished
+        thread.join()
+    print("hier")
