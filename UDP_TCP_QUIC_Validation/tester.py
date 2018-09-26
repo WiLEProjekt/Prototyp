bcee8022405735050452221191b7a372a944b3c4
Dennis <dennis.rieke.93@gmail.com>
2018-09-26 22:19:02 +0200

    tester verbessert

--- UDP_TCP_QUIC_Validation/tester.py
+++ UDP_TCP_QUIC_Validation/tester.py
@@ -52,276 +52,276 @@ def write_result(filename, result):
 
 
 def main():
-    tcp_filename_down = "tcp.json"
-    udp_filename_down = "udp.json"
-    udp_plus_filename_down = "udppluss.json"
-    udp_minus_filename_down = "udpminus.json"
+    tcp_filename_u = "tcp.json"
+    udp_filename_u = "udp.json"
+    udp_plus_filename_u = "udppluss.json"
+    udp_minus_filename_u = "udpminus.json"
 
-    tcp_filename_up = "tcpup.json"
-    udp_filename_up = "udpup.json"
-    udp_plus_filename_up = "udpplussup.json"
-    udp_minus_filename_up = "udpminusup.json"
+    tcp_filename_d = "tcpup.json"
+    udp_filename_d = "udpup.json"
+    udp_plus_filename_d = "udpplussup.json"
+    udp_minus_filename_d = "udpminusup.json"
 
-    tcp_filename_down5 = "tcp5.json"
-    udp_filename_down5 = "udp5.json"
-    udp_plus_filename_down5 = "udppluss5.json"
-    udp_minus_filename_down5 = "udpminus5.json"
+    tcp_filename_u5 = "tcp5.json"
+    udp_filename_u5 = "udp5.json"
+    udp_plus_filename_u5 = "udppluss5.json"
+    udp_minus_filename_u5 = "udpminus5.json"
 
-    tcp_filename_up5 = "tcpup5.json"
-    udp_filename_up5 = "udpup5.json"
-    udp_plus_filename_up5 = "udpplussup5.json"
-    udp_minus_filename_up5 = "udpminusup5.json"
+    tcp_filename_d5 = "tcpup5.json"
+    udp_filename_d5 = "udpup5.json"
+    udp_plus_filename_d5 = "udpplussup5.json"
+    udp_minus_filename_d5 = "udpminusup5.json"
 
-    tcp_filename_down15 = "tcp15.json"
-    udp_filename_down15 = "udp15.json"
-    udp_plus_filename_down15 = "udppluss15.json"
-    udp_minus_filename_down15 = "udpminus15.json"
+    tcp_filename_u15 = "tcp15.json"
+    udp_filename_u15 = "udp15.json"
+    udp_plus_filename_u15 = "udppluss15.json"
+    udp_minus_filename_u15 = "udpminus15.json"
 
-    tcp_filename_up15 = "tcpup15.json"
-    udp_filename_up15 = "udpup15.json"
-    udp_plus_filename_up15 = "udpplussup15.json"
-    udp_minus_filename_up15 = "udpminusup15.json"
+    tcp_filename_d15 = "tcpup15.json"
+    udp_filename_d15 = "udpup15.json"
+    udp_plus_filename_d15 = "udpplussup15.json"
+    udp_minus_filename_d15 = "udpminusup15.json"
 
-    result_tcp = []
-    result_udp = []
-    result_udp_plus = []
-    result_udp_minus = []
+    result_tcp_u = []
+    result_udp_u = []
+    result_udp_plus_u = []
+    result_udp_minus_u = []
 
-    result_tcp_up = []
-    result_udp_up = []
-    result_udp_plus_up = []
-    result_udp_minus_up = []
+    result_tcp_d = []
+    result_udp_d = []
+    result_udp_plus_d = []
+    result_udp_minus_d = []
 
-    result_tcp5 = []
-    result_udp5 = []
-    result_udp_plus5 = []
-    result_udp_minus5 = []
+    result_tcp5_u = []
+    result_udp5_u = []
+    result_udp_plus5_u = []
+    result_udp_minus5_u = []
 
-    result_tcp_up5 = []
-    result_udp_up5 = []
-    result_udp_plus_up5 = []
-    result_udp_minus_up5 = []
+    result_tcp_5_d = []
+    result_udp_5_d = []
+    result_udp_plus_5_d = []
+    result_udp_minus_5_d = []
 
 
-    result_tcp15 = []
-    result_udp15 = []
-    result_udp_plus15 = []
-    result_udp_minus15 = []
+    result_tcp15_u = []
+    result_udp15_u = []
+    result_udp_plus15_u = []
+    result_udp_minus15_u = []
 
-    result_tcp_up15 = []
-    result_udp_up15 = []
-    result_udp_plus_up15 = []
-    result_udp_minus_up15 = []
+    result_tcp_15_d = []
+    result_udp_15_d = []
+    result_udp_plus_15_d = []
+    result_udp_minus_15_d = []
 
     for i in range(10):
         try:
-            os.remove(tcp_filename_down)
+            os.remove(tcp_filename_u)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_filename_down)
+            os.remove(udp_filename_u)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_plus_filename_down)
+            os.remove(udp_plus_filename_u)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_minus_filename_down)
+            os.remove(udp_minus_filename_u)
         except FileNotFoundError:
             pass
         try:
-            os.remove(tcp_filename_up)
+            os.remove(tcp_filename_d)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_filename_up)
+            os.remove(udp_filename_d)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_plus_filename_up)
+            os.remove(udp_plus_filename_d)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_minus_filename_up)
+            os.remove(udp_minus_filename_d)
         except FileNotFoundError:
             pass
 
         try:
-            os.remove(tcp_filename_down5)
+            os.remove(tcp_filename_u5)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_filename_down5)
+            os.remove(udp_filename_u5)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_plus_filename_down5)
+            os.remove(udp_plus_filename_u5)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_minus_filename_down5)
+            os.remove(udp_minus_filename_u5)
         except FileNotFoundError:
             pass
         try:
-            os.remove(tcp_filename_up5)
+            os.remove(tcp_filename_d5)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_filename_up5)
+            os.remove(udp_filename_d5)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_plus_filename_up5)
+            os.remove(udp_plus_filename_d5)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_minus_filename_up5)
+            os.remove(udp_minus_filename_d5)
         except FileNotFoundError:
             pass
 
         try:
-            os.remove(tcp_filename_down15)
+            os.remove(tcp_filename_u15)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_filename_down15)
+            os.remove(udp_filename_u15)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_plus_filename_down15)
+            os.remove(udp_plus_filename_u15)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_minus_filename_down15)
+            os.remove(udp_minus_filename_u15)
         except FileNotFoundError:
             pass
         try:
-            os.remove(tcp_filename_up15)
+            os.remove(tcp_filename_d15)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_filename_up15)
+            os.remove(udp_filename_d15)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_plus_filename_up15)
+            os.remove(udp_plus_filename_d15)
         except FileNotFoundError:
             pass
         try:
-            os.remove(udp_minus_filename_up15)
+            os.remove(udp_minus_filename_d15)
         except FileNotFoundError:
             pass
 
-        os.system("iperf3 -c 131.173.33.228 -p 50000 -J --logfile " + tcp_filename_down)
-        result_tcp.append(read_tcp_bandwidth(tcp_filename_down))
+        os.system("iperf3 -c 131.173.33.228 -p 50000 -J --logfile " + tcp_filename_u)
+        result_tcp_u.append(read_tcp_bandwidth(tcp_filename_u))
 
-        os.system("iperf3 -c 131.173.33.228 -p 50000 -u -b " + str(int(result_tcp[i])) + " -l 1400 -J --logfile " + udp_filename_down)
-        result_udp.append(read_udp_bandwidth(udp_filename_down))
+        os.system("iperf3 -c 131.173.33.228 -p 50000 -u -b " + str(int(result_tcp_u[i])) + " -l 1400 -J --logfile " + udp_filename_u)
+        result_udp_u.append(read_udp_bandwidth(udp_filename_u))
 
-        os.system("iperf3 -c 131.173.33.228 -p 50000 -u -b " + str(int(result_tcp[i] * 1.1)) + " -l 1400 -J --logfile " + udp_plus_filename_down)
-        result_udp_plus.append(read_udp_bandwidth(udp_plus_filename_down))
+        os.system("iperf3 -c 131.173.33.228 -p 50000 -u -b " + str(int(result_tcp_u[i] * 1.1)) + " -l 1400 -J --logfile " + udp_plus_filename_u)
+        result_udp_plus_u.append(read_udp_bandwidth(udp_plus_filename_u))
 
-        os.system("iperf3 -c 131.173.33.228 -p 50000 -u -b " + str(int(result_tcp[i] / 1.1)) + " -l 1400 -J --logfile " + udp_minus_filename_down)
-        result_udp_minus.append(read_udp_bandwidth(udp_minus_filename_down))
+        os.system("iperf3 -c 131.173.33.228 -p 50000 -u -b " + str(int(result_tcp_u[i] / 1.1)) + " -l 1400 -J --logfile " + udp_minus_filename_u)
+        result_udp_minus_u.append(read_udp_bandwidth(udp_minus_filename_u))
 
-        os.system("iperf3 -c 131.173.33.228 -p 50000 -R -J --logfile " + tcp_filename_up)
-        result_tcp_up.append(read_tcp_bandwidth(tcp_filename_up))
+        os.system("iperf3 -c 131.173.33.228 -p 50000 -R -J --logfile " + tcp_filename_d)
+        result_tcp_d.append(read_tcp_bandwidth(tcp_filename_d))
 
         os.system("iperf3 -c 131.173.33.228 -p 50000 -R -u -b " + str(
-            int(result_tcp[i])) + " -l 1400 -J --logfile " + udp_filename_up)
-        result_udp_up.append(read_udp_bandwidth(udp_filename_up))
+            int(result_tcp_d[i])) + " -l 1400 -J --logfile " + udp_filename_d)
+        result_udp_d.append(read_udp_bandwidth(udp_filename_d))
 
         os.system("iperf3 -c 131.173.33.228 -p 50000 -R -u -b " + str(
-            int(result_tcp[i] * 1.1)) + " -l 1400 -J --logfile " + udp_plus_filename_up)
-        result_udp_plus_up.append(read_udp_bandwidth(udp_plus_filename_up))
+            int(result_tcp_d[i] * 1.1)) + " -l 1400 -J --logfile " + udp_plus_filename_d)
+        result_udp_plus_d.append(read_udp_bandwidth(udp_plus_filename_d))
 
         os.system("iperf3 -c 131.173.33.228 -p 50000 -R -u -b " + str(
-            int(result_tcp[i] / 1.1)) + " -l 1400 -J --logfile " + udp_minus_filename_up)
-        result_udp_minus_up.append(read_udp_bandwidth(udp_minus_filename_up))
+            int(result_tcp_d[i] / 1.1)) + " -l 1400 -J --logfile " + udp_minus_filename_d)
+        result_udp_minus_d.append(read_udp_bandwidth(udp_minus_filename_d))
 
 
-        os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -J --logfile " + tcp_filename_down5)
-        result_tcp5.append(read_tcp_bandwidth(tcp_filename_down5))
+        os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -J --logfile " + tcp_filename_u5)
+        result_tcp5_u.append(read_tcp_bandwidth(tcp_filename_u5))
 
-        os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -u -b " + str(int(result_tcp[i])) + " -l 1400 -J --logfile " + udp_filename_down5)
-        result_udp5.append(read_udp_bandwidth(udp_filename_down5))
+        os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -u -b " + str(int(result_tcp5_u[i])) + " -l 1400 -J --logfile " + udp_filename_u5)
+        result_udp5_u.append(read_udp_bandwidth(udp_filename_u5))
 
-        os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -u -b " + str(int(result_tcp[i] * 1.1)) + " -l 1400 -J --logfile " + udp_plus_filename_down5)
-        result_udp_plus5.append(read_udp_bandwidth(udp_plus_filename_down5))
+        os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -u -b " + str(int(result_tcp5_u[i] * 1.1)) + " -l 1400 -J --logfile " + udp_plus_filename_u5)
+        result_udp_plus5_u.append(read_udp_bandwidth(udp_plus_filename_u5))
 
-        os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -u -b " + str(int(result_tcp[i] / 1.1)) + " -l 1400 -J --logfile " + udp_minus_filename_down5)
-        result_udp_minus5.append(read_udp_bandwidth(udp_minus_filename_down5))
+        os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -u -b " + str(int(result_tcp5_u[i] / 1.1)) + " -l 1400 -J --logfile " + udp_minus_filename_u5)
+        result_udp_minus5_u.append(read_udp_bandwidth(udp_minus_filename_u5))
 
-        os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -R -J --logfile " + tcp_filename_up5)
-        result_tcp_up5.append(read_tcp_bandwidth(tcp_filename_up5))
-
-        os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -R -u -b " + str(
-            int(result_tcp[i])) + " -l 1400 -J --logfile " + udp_filename_up5)
-        result_udp_up5.append(read_udp_bandwidth(udp_filename_up5))
+        os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -R -J --logfile " + tcp_filename_d5)
+        result_tcp_5_d.append(read_tcp_bandwidth(tcp_filename_d5))
 
         os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -R -u -b " + str(
-            int(result_tcp[i] * 1.1)) + " -l 1400 -J --logfile " + udp_plus_filename_up5)
-        result_udp_plus_up5.append(read_udp_bandwidth(udp_plus_filename_up5))
+            int(result_tcp_5_d[i])) + " -l 1400 -J --logfile " + udp_filename_d5)
+        result_udp_5_d.append(read_udp_bandwidth(udp_filename_d5))
 
         os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -R -u -b " + str(
-            int(result_tcp[i] / 1.1)) + " -l 1400 -J --logfile " + udp_minus_filename_up5)
-        result_udp_minus_up5.append(read_udp_bandwidth(udp_minus_filename_up5))
+            int(result_tcp_5_d[i] * 1.1)) + " -l 1400 -J --logfile " + udp_plus_filename_d5)
+        result_udp_plus_5_d.append(read_udp_bandwidth(udp_plus_filename_d5))
+
+        os.system("iperf3 -c 131.173.33.228 -t 5 -p 50000 -R -u -b " + str(
+            int(result_tcp_5_d[i] / 1.1)) + " -l 1400 -J --logfile " + udp_minus_filename_d5)
+        result_udp_minus_5_d.append(read_udp_bandwidth(udp_minus_filename_d5))
 
 
-        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -J --logfile " + tcp_filename_down15)
-        result_tcp15.append(read_tcp_bandwidth(tcp_filename_down15))
+        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -J --logfile " + tcp_filename_u15)
+        result_tcp15_u.append(read_tcp_bandwidth(tcp_filename_u15))
 
-        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -u -b " + str(int(result_tcp[i])) + " -l 1400 -J --logfile " + udp_filename_down15)
-        result_udp15.append(read_udp_bandwidth(udp_filename_down15))
+        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -u -b " + str(int(result_tcp15_u[i])) + " -l 1400 -J --logfile " + udp_filename_u15)
+        result_udp15_u.append(read_udp_bandwidth(udp_filename_u15))
 
-        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -u -b " + str(int(result_tcp[i] * 1.1)) + " -l 1400 -J --logfile " + udp_plus_filename_down15)
-        result_udp_plus15.append(read_udp_bandwidth(udp_plus_filename_down15))
+        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -u -b " + str(int(result_tcp15_u[i] * 1.1)) + " -l 1400 -J --logfile " + udp_plus_filename_u15)
+        result_udp_plus15_u.append(read_udp_bandwidth(udp_plus_filename_u15))
 
-        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -u -b " + str(int(result_tcp[i] / 1.1)) + " -l 1400 -J --logfile " + udp_minus_filename_down15)
-        result_udp_minus15.append(read_udp_bandwidth(udp_minus_filename_down15))
+        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -u -b " + str(int(result_tcp15_u[i] / 1.1)) + " -l 1400 -J --logfile " + udp_minus_filename_u15)
+        result_udp_minus15_u.append(read_udp_bandwidth(udp_minus_filename_u15))
 
-        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -R -J --logfile " + tcp_filename_up15)
-        result_tcp_up15.append(read_tcp_bandwidth(tcp_filename_up15))
-
-        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -R -u -b " + str(
-            int(result_tcp[i])) + " -l 1400 -J --logfile " + udp_filename_up15)
-        result_udp_up15.append(read_udp_bandwidth(udp_filename_up15))
-
-        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -R -u -b " + str(
-            int(result_tcp[i] * 1.1)) + " -l 1400 -J --logfile " + udp_plus_filename_up15)
-        result_udp_plus_up15.append(read_udp_bandwidth(udp_plus_filename_up15))
+        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -R -J --logfile " + tcp_filename_d15)
+        result_tcp_15_d.append(read_tcp_bandwidth(tcp_filename_d15))
 
         os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -R -u -b " + str(
-            int(result_tcp[i] / 1.1)) + " -l 1400 -J --logfile " + udp_minus_filename_up15)
-        result_udp_minus_up15.append(read_udp_bandwidth(udp_minus_filename_up15))
+            int(result_tcp_15_d[i])) + " -l 1400 -J --logfile " + udp_filename_d15)
+        result_udp_15_d.append(read_udp_bandwidth(udp_filename_d15))
 
-    write_result("tcp.csv", result_tcp)
-    write_result("udp.csv", result_udp)
-    write_result("udpPlus.csv", result_udp_plus)
-    write_result("udpMinus.csv", result_udp_minus)
-    write_result("tcpup.csv", result_tcp_up)
-    write_result("udpup.csv", result_udp_up)
-    write_result("udpPlusup.csv", result_udp_plus_up)
-    write_result("udpMinusup.csv", result_udp_minus_up)
+        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -R -u -b " + str(
+            int(result_tcp_15_d[i] * 1.1)) + " -l 1400 -J --logfile " + udp_plus_filename_d15)
+        result_udp_plus_15_d.append(read_udp_bandwidth(udp_plus_filename_d15))
 
-    write_result("tcp5.csv", result_tcp5)
-    write_result("udp5.csv", result_udp5)
-    write_result("udpPlus5.csv", result_udp_plus5)
-    write_result("udpMinus5.csv", result_udp_minus5)
-    write_result("tcpup5.csv", result_tcp_up5)
-    write_result("udpup5.csv", result_udp_up5)
-    write_result("udpPlusup5.csv", result_udp_plus_up5)
-    write_result("udpMinusup5.csv", result_udp_minus_up5)
+        os.system("iperf3 -c 131.173.33.228 -t 15 -p 50000 -R -u -b " + str(
+            int(result_tcp_15_d[i] / 1.1)) + " -l 1400 -J --logfile " + udp_minus_filename_d15)
+        result_udp_minus_15_d.append(read_udp_bandwidth(udp_minus_filename_d15))
 
-    write_result("tcp15.csv", result_tcp15)
-    write_result("udp15.csv", result_udp15)
-    write_result("udpPlus15.csv", result_udp_plus15)
-    write_result("udpMinus15.csv", result_udp_minus15)
-    write_result("tcpup15.csv", result_tcp_up15)
-    write_result("udpup15.csv", result_udp_up15)
-    write_result("udpPlusup15.csv", result_udp_plus_up15)
-    write_result("udpMinusup15.csv", result_udp_minus_up15)
+    write_result("tcpu.csv", result_tcp_u)
+    write_result("udpu.csv", result_udp_u)
+    write_result("udpPlusu.csv", result_udp_plus_u)
+    write_result("udpMinusu.csv", result_udp_minus_u)
+    write_result("tcpd.csv", result_tcp_d)
+    write_result("udpd.csv", result_udp_d)
+    write_result("udpPlusd.csv", result_udp_plus_d)
+    write_result("udpMinusd.csv", result_udp_minus_d)
+
+    write_result("tcp5u.csv", result_tcp5_u)
+    write_result("udp5u.csv", result_udp5_u)
+    write_result("udpPlus5u.csv", result_udp_plus5_u)
+    write_result("udpMinus5u.csv", result_udp_minus5_u)
+    write_result("tcp5d.csv", result_tcp_5_d)
+    write_result("udp5d.csv", result_udp_5_d)
+    write_result("udpPlus5d.csv", result_udp_plus_5_d)
+    write_result("udpMinus5d.csv", result_udp_minus_5_d)
+
+    write_result("tcp15u.csv", result_tcp15_u)
+    write_result("udp15u.csv", result_udp15_u)
+    write_result("udpPlus15u.csv", result_udp_plus15_u)
+    write_result("udpMinus15u.csv", result_udp_minus15_u)
+    write_result("tcp15d.csv", result_tcp_15_d)
+    write_result("udp15d.csv", result_udp_15_d)
+    write_result("udpPlus15d.csv", result_udp_plus_15_d)
+    write_result("udpMinus15d.csv", result_udp_minus_15_d)
 
 
 if __name__ == "__main__":
