d717751977585f86e1a0694ce0328f45552ecc39
Dennis Rieke <dennis.rieke.93@gmail.com>
2018-10-12 01:14:46 +0200

    Add files via upload

--- Auswertungstool/PcapReader/main.cpp
+++ Auswertungstool/PcapReader/main.cpp
@@ -3,15 +3,22 @@
 #include <sys/stat.h>
 #include <cmath>
 #include <vector>
+#include <algorithm>
 #include <map>
+#include <fstream>
 
 using namespace std;
 
 const unsigned int MAX_SEQ_NUM = 65535;
 
-struct results {
-    vector<struct timeval> delays;
+struct result {
     vector<bool> loss;
+    vector<struct timeval> delays;
+};
+
+struct pcapValues {
+    map<unsigned long, struct timeval> send;
+    map<unsigned long, struct timeval> recieved;
 };
 
 unsigned int parseNumberFromBytes(const unsigned char *bytes, int length) {
@@ -19,7 +26,6 @@ unsigned int parseNumberFromBytes(const unsigned char *bytes, int length) {
     for (int i = length - 1; i >= 0; i--) {
         number += bytes[length - i - 1] * pow(256, i);
     }
-
     return number;
 }
 
@@ -29,84 +35,70 @@ string parseIP(unsigned char *bytes) {
         unsigned char numberByte[] = {bytes[i]};
         unsigned int number = parseNumberFromBytes(numberByte, 1);
         ip += std::to_string(number);
-        if (i < 4) {
+        if (i < 3) {
             ip += ".";
         }
     }
     return ip;
 }
 
-int getSeqNum(const u_char *data) {
+unsigned long getSeqNum(const u_char *data) {
     //Data start: 42
-    unsigned char seqNumBytes[] = {data[42], data[43]};
-    return parseNumberFromBytes(seqNumBytes, 2);
+    unsigned char seqNumBytes[] = {data[50], data[51], data[52], data[53]};
+    unsigned long seqNum = parseNumberFromBytes(seqNumBytes, 4);
+    return seqNum;
 }
 
-struct timeval findResponse(int seqNum, const map<int, struct timeval> &responseTimestamps) {
-    return responseTimestamps.at(seqNum);
+vector<bool> getLoss(vector<unsigned long> sendSeqNums, vector<unsigned long> recievedSeqNums) {
+    vector<bool> trace;
+    for(unsigned long send : sendSeqNums){
+        if(std::find(recievedSeqNums.begin(), recievedSeqNums.end(), send) != recievedSeqNums.end()){
+            trace.push_back(true);
+        } else {
+            trace.push_back(false);
+        }
+    }
+    return trace;
 }
 
-vector<struct timeval>
-getDelays(map<int, struct timeval> requestTimestamps, const map<int, struct timeval> &responseTimestamps) {
+void printData(const u_char *data, struct pcap_pkthdr *header){
+    cout << endl;
+    for(u_int i = 0; (i < header->caplen); i++){
+        if((i%16) == 0){
+            cout << endl;
+        }
+        printf("%.2x ", data[i]);
+    }
+}
+
+struct result getDelays(map<unsigned long, struct timeval> sendTS, map<unsigned long, struct timeval> recievedTS){
+    struct result results {};
     vector<struct timeval> delays;
-    for (const auto &iterator : requestTimestamps) {
-        struct timeval delay{};
-        struct timeval responseTimestamp = findResponse(iterator.first, responseTimestamps);
-        delay.tv_sec = (responseTimestamp.tv_sec - iterator.second.tv_sec) / 2;
-        delay.tv_usec = (responseTimestamp.tv_usec - iterator.second.tv_usec) / 2;
+    vector<bool> loss;
+    for(auto& send: sendTS){
+        struct timeval delay {};
+        auto recieved = recievedTS.find(send.first);
+        if(recieved != recievedTS.end()) {
+            delay.tv_sec = recieved->second.tv_sec - send.second.tv_sec;
+            delay.tv_usec = recieved->second.tv_usec - send.second.tv_usec;
+            loss.push_back(true);
+        } else {
+            delay.tv_sec = -1;
+            delay.tv_usec = -1;
+            loss.push_back(false);
+        }
         delays.push_back(delay);
     }
-
-    return delays;
+    results.delays = delays;
+    results.loss = loss;
+    return results;
 }
 
-vector<bool> findMissingSeqNums(vector<unsigned int> sequenzNumbers) {
-    vector<bool> calculatedLosses;
-    unsigned int lastSeqNum = 0; //important initialization => detects if first few packages are lost
-    unsigned long packetCounter = 0;
-    bool firstSeqNumRed = false;
+struct pcapValues readPcapFile(const string &filename, const string &ipOfPcapDevice, const string &ipOfOtherDevice) {
+    map<unsigned long, struct timeval> toOtherTS;
+    map<unsigned long, struct timeval> toSelfTS;
 
-    for (unsigned int seqNum : sequenzNumbers) {
-        if (firstSeqNumRed) {
-            int diff = seqNum - lastSeqNum;
-            if (diff > 1) { //packets are lost in ascending order, maximum sequencenumber is not yet reached
-                for (int i = 0; i < diff - 1; i++) { //Push lost packets
-                    packetCounter++;
-                    calculatedLosses.push_back(false);
-                }
-                calculatedLosses.push_back(true); //Push found packet
-                packetCounter++;
-            } else if (diff <= 0) { //Packet loss at maximum sequencenumber detected
-                diff = MAX_SEQ_NUM - lastSeqNum + seqNum;
-                for (int i = 0; i < diff; i++) {
-                    packetCounter++;
-                    calculatedLosses.push_back(false);
-                }
-                calculatedLosses.push_back(true); //push found packet
-                packetCounter++;
-            } else { //diff = 1 = no packet loss
-                packetCounter++;
-                calculatedLosses.push_back(true);
-            }
-        } else {
-            calculatedLosses.push_back(true);
-        }
-        firstSeqNumRed = true;
-        lastSeqNum = seqNum;
-    }
-
-    return calculatedLosses;
-}
-
-vector<bool> getLoss(vector<unsigned int> seqNums) {
-    vector<bool> trace;
-    trace = findMissingSeqNums(move(seqNums));
-}
-
-struct results readPcapFile(const string &filename, const string &sourceIp, const string &destIp) {
-    map<int, struct timeval> requestTimestamps;
-    map<int, struct timeval> responseTimestamps;
-    vector<unsigned int> seqNums;
+    struct pcapValues result{};
 
     struct stat buffer{};
     if (stat(filename.c_str(), &buffer) != 0) {
@@ -116,11 +108,14 @@ struct results readPcapFile(const string &filename, const string &sourceIp, cons
         pcap_t *pcap = pcap_open_offline(filename.c_str(), errbuff);
         struct pcap_pkthdr *header;
         const u_char *data;
-
+        bool isFirstPackageToOther = true;
+        bool isFirstPackageToSelf = true;
+        unsigned long packageCount = 0;
         while (pcap_next_ex(pcap, &header, &data) >= 0) {
             if (header->len != header->caplen) {
-                printf("Warning! Capture size different than packet size: %1d bytes\n", header->caplen);
+                //printf("Warning! Capture size different than packet size: %1d bytes\n", header->caplen);
             }
+            packageCount++;
 
             unsigned char sourceIPBytes[] = {data[26], data[27], data[28], data[29]};
             unsigned char destIPBytes[] = {data[30], data[31], data[32], data[33]};
@@ -128,39 +123,105 @@ struct results readPcapFile(const string &filename, const string &sourceIp, cons
             string packetDest = parseIP(destIPBytes);
 
             //check if packet is UDP (17)
-            if (data[24] == 17) {
-                if (sourceIp == parseIP(sourceIPBytes) && destIp == parseIP(destIPBytes)) {
-                    requestTimestamps[getSeqNum(data)] = header->ts;
-                } else if (sourceIp == packetDest && destIp == packetSource) {
-                    responseTimestamps[getSeqNum(data)] = header->ts;
+            unsigned char lengthBytes[] = {data[16], data[17]};
+            unsigned long length = parseNumberFromBytes(lengthBytes, 2);
+            if (length == 1478) {
+                unsigned long seqNum = getSeqNum(data);
+                //printData(data, header);
+                if (ipOfPcapDevice == packetSource && ipOfOtherDevice == packetDest) {
+                    if(isFirstPackageToOther) {
+                        isFirstPackageToOther = false;
+                    } else {
+                        toOtherTS[seqNum] = header->ts;
+                    }
+                } else if (ipOfPcapDevice == packetDest && ipOfOtherDevice == packetSource) {
+                    if(isFirstPackageToSelf) {
+                        isFirstPackageToSelf = false;
+                    } else {
+                        toSelfTS[seqNum] = header->ts;
+                    }
                 }
-                unsigned char seqNumBytes[] = {data[42], data[43]};
-                seqNums.push_back(parseNumberFromBytes(seqNumBytes, 2));
             }
         }
         pcap_close(pcap);
 
-        vector<struct timeval> delays = getDelays(requestTimestamps, responseTimestamps);
-        vector<bool> lossTrace = getLoss(seqNums);
-        struct results result{};
-        result.delays = delays;
-        result.loss = lossTrace;
+        result.send = toOtherTS;
+        result.recieved = toSelfTS;
 
         return result;
     }
 
-    return nullptr;
+    return result;
+}
+
+void printResult(const vector<bool> &clientToServerLoss, const vector<bool> &serverToClientLoss,
+                 const vector<timeval> &clientToServerDelays, const vector<timeval> &serverToClientDelays) {
+    cout << "Loss Client to Server" << endl;
+    for(bool b : clientToServerLoss){
+        cout << b << " ";
+    }
+    cout << endl << "Loss Server to Client" << endl;
+    for(bool b : serverToClientLoss) {
+        cout << b << " ";
+    }
+    cout << endl << "Delays Client to Server:" << endl;
+    for(struct timeval tv : clientToServerDelays){
+        cout << tv.tv_sec << "." << tv.tv_usec << " ";
+    }
+    cout << endl << "Delays Server to Client:" << endl;
+    for(struct timeval tv : serverToClientDelays){
+        cout << tv.tv_sec << "." << tv.tv_usec << " ";
+    }
+}
+
+void writeResultToFile(result upload, result download) {
+    ofstream uploadLossFile;
+    uploadLossFile.open("uploadLoss.txt");
+    for(bool b : upload.loss){
+        uploadLossFile << b;
+    }
+    uploadLossFile.close();
+
+    ofstream downloadLossFile;
+    uploadLossFile.open("downloadLoss.txt");
+    for(bool b : download.loss){
+        downloadLossFile << b;
+    }
+    downloadLossFile.close();
+
+    ofstream uploadDelayFile;
+    uploadDelayFile.open("uploadDelays.csv");
+    for(struct timeval tv : upload.delays){
+        uploadDelayFile << tv.tv_sec << "." << tv.tv_usec << ";";
+    }
+    uploadDelayFile.close();
+
+    ofstream downloadDelayFile;
+    downloadDelayFile.open("downloadDelays.csv");
+    for(struct timeval tv : download.delays){
+        downloadDelayFile << tv.tv_sec << "." << tv.tv_usec << ";";
+    }
+    downloadDelayFile.close();
 }
 
 int main(int argc, char **argv) {
     if (argc < 4) {
-        cout << "PcapReader [filename] [sourceIp] [destIp]";
+        cout << "PcapReader [client.pcap] [server.pcap] [serverIp] [locale clientIp] [global clientIp]";
         return -1;
     }
-    string filename = argv[1];
-    string sourceIp = argv[2];
-    string destIp = argv[3];
-    struct results result = readPcapFile(filename, sourceIp, destIp);
-    
+    string clientFilename = argv[1];
+    string serverFilename = argv[2];
+    string serverIp = argv[3];
+    string localeClientIp = argv[4];
+    string globalClientIp = argv[5];
+    struct pcapValues clientResult = readPcapFile(clientFilename, localeClientIp, serverIp);
+    struct pcapValues serverResult = readPcapFile(serverFilename, serverIp, globalClientIp);
+
+    struct result uploadResult = getDelays(clientResult.send, serverResult.recieved);
+    struct result downloadResult = getDelays(serverResult.send, clientResult.recieved);
+
+    //printResult(uploadLoss, downloadLoss, uploadDelays, downloadDelays);
+    writeResultToFile(uploadResult, downloadResult);
+
     return 0;
 }
