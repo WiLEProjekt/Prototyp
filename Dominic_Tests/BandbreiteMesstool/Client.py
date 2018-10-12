7e81994879ee8ad52ab956875f491849c5ef19f9
dlanie <d_lani01@uni-muenster.de>
2018-10-12 10:15:51 +0200

    added signal strength

--- Dominic_Tests/BandbreiteMesstool/Client.py
+++ Dominic_Tests/BandbreiteMesstool/Client.py
@@ -1,6 +1,58 @@
-import sys, getopt, os, json, socket, time, multiprocessing, pcapy, signal
+import sys, getopt, os, json, socket, time, multiprocessing, pcapy, signal, pprint, requests, xmltodict
 from datetime import datetime
 from threading import Thread
+import threading
+
+class HuaweiE3372(object):
+  BASE_URL = 'http://{host}'
+  COOKIE_URL = '/html/index.html'
+  XML_APIS = [
+    '/api/device/information',
+    '/api/device/signal',
+    '/api/monitoring/status',
+    '/api/monitoring/traffic-statistics',
+    '/api/dialup/connection',
+    '/api/global/module-switch',
+    '/api/net/current-plmn',
+    '/api/net/net-mode',
+  ]
+  session = None
+
+  def __init__(self,host='192.168.8.1'):
+    self.host = host
+    self.base_url = self.BASE_URL.format(host=host)
+    self.session = requests.Session()
+    # get a session cookie by requesting the COOKIE_URL
+    r = self.session.get(self.base_url + self.COOKIE_URL)
+
+  def get(self,path):
+    return xmltodict.parse(self.session.get(self.base_url + path).text).get('response',None)
+
+def signalstrength(kill):
+    e3372 = HuaweiE3372()
+    print("<data>")
+    while not kill:
+        print("<set time=", time.time(), ">")
+
+        for path in e3372.XML_APIS:
+            for key, value in e3372.get(path).items():
+
+                # print(key,value)
+                if (key == u'FullName'):
+                    print("<fullname>", value, "</fullname>")
+                if (key == u'workmode'):
+                    print("<workmode>", value, "</workmode>")
+                if (key == u'rsrq'):
+                    print("<rsrq>", value, "</rsrq>")
+                if (key == u'rssi'):
+                    print("<rssi>", value, "</rssi>")
+                if (key == u'sinr'):
+                    print("<sinr>", value, "</sinr>")
+                if (key == u'rsrp'):
+                    print("<rsrp>", value, "</rsrp>")
+        print("</set>")
+        time.sleep(1)
+    print("</data>") #end
 
 def uploadBandwidth():
     try:
@@ -91,6 +143,9 @@ def main(argv):
     destIP = "131.173.33.228"
     destPort = 50001
 
+    signalkill = threading.Event()
+    signalthread = Thread(target=signalstrength, args=(signalkill))
+    signalthread.start()
     ################################
     # TCP Bandwidth Upload Measurement
     ################################
@@ -160,6 +215,9 @@ def main(argv):
     time.sleep(1)
 
     tcpsock.close()
+
+    signalkill.set()
+    signalthread.join()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
