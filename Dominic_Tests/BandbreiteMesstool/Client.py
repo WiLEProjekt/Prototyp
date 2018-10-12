24abd844bd6bd8a66bab12c466901d285a22fa5d
dlanie <d_lani01@uni-muenster.de>
2018-10-12 11:23:50 +0200

    ..

--- Dominic_Tests/BandbreiteMesstool/Client.py
+++ Dominic_Tests/BandbreiteMesstool/Client.py
@@ -40,17 +40,17 @@ def signalstrength(kill, measurementId):
 
                 # print(key,value)
                 if (key == u'FullName'):
-                    file.write("<fullname>", value, "</fullname>")
+                    file.write("<fullname>" + str(value) + "</fullname>")
                 if (key == u'workmode'):
-                    file.write("<workmode>", value, "</workmode>")
+                    file.write("<workmode>" + str(value) + "</workmode>")
                 if (key == u'rsrq'):
-                    file.write("<rsrq>", value, "</rsrq>")
+                    file.write("<rsrq>" + str(value) + "</rsrq>")
                 if (key == u'rssi'):
-                    file.write("<rssi>", value, "</rssi>")
+                    file.write("<rssi>" + str(value) + "</rssi>")
                 if (key == u'sinr'):
-                    file.write("<sinr>", value, "</sinr>")
+                    file.write("<sinr>" + str(value) + "</sinr>")
                 if (key == u'rsrp'):
-                    file.write("<rsrp>", value, "</rsrp>")
+                    file.write("<rsrp>" + str(value) + "</rsrp>")
                 file.write("</set>")
         time.sleep(1)
     file.write("</data>") #end
