358de25fa06670db1e0f2c2efdbdae50953bf6cc
Dominic <dlaniewski@uos.de>
2018-12-03 15:35:31 +0100

    added pcap for server

--- Messtool/MobileMesurementTool/Server.py
+++ Messtool/MobileMesurementTool/Server.py
@@ -9,6 +9,30 @@ packetsize = 1 # will be overwritten by first packet received
 bandwidth = 1 # will be overwritten by first packet received
 clientIP=""
 
+def signal_term_handler(signal, frame):
+    sys.exit(0)
+
+
+def write_pcap(filename, interface):
+    pcapy.findalldevs()
+    max_bytes = 1024
+    promiscuous = False
+    read_timeout = 100
+    pc = pcapy.open_live(interface, max_bytes, promiscuous, read_timeout)
+    dumper = pc.dump_open(filename + ".pcap")
+    signal.signal(signal.SIGTERM, signal_term_handler)
+
+    try:
+        pc.setfilter('udp')
+
+        def recv_pkts(hdr, data):
+            dumper.dump(hdr, data)
+
+        packet_limit = -1  # infinite
+        pc.loop(packet_limit, recv_pkts)
+    except BaseException:
+        pc.close()
+
 def calcSequencenumber(sequencenumber):
     if (sequencenumber + 1) == sys.maxsize: #start with 0 if max int is reached
         return(0)
@@ -59,8 +83,9 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     ownIP = ""
     ownPort = 5000
+    interface=""
     try:
-        opts, args = getopt.getopt(argv, "hi:p:", ["Server IP","Port"])
+        opts, args = getopt.getopt(argv, "hi:p:n:", ["Server IP","Port","Interface"])
     except getopt.GetoptError:
         print("Usage: python3 Server.py -i <Server IP> -p <Port>")
         sys.exit(2)
@@ -72,14 +97,23 @@ if __name__ == "__main__":
             ownIP = arg
         elif opt in ("-p", "--port"):
             ownPort = int(arg)
-    if ownIP == "":
-        print("The IP address this server should listen on is missing")
+        elif opt in ("-n", "--interface"):
+            interface = arg
+    if ownIP == "" or interface == "":
+        print("The IP address this server should listen on is missing or no Interface provided")
         sys.exit(2)
 
+    currenttime = str(datetime.now())
+    currenttime = currenttime.replace(':', '_')
+    currenttime = currenttime.replace('.', '_')
+    currenttime = currenttime.replace(' ', '_')
+    measurementID = currenttime + "_Train_MS_OS"
     udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
     udpsock.bind((ownIP, ownPort))
     udpsock.settimeout(2)
     print("Server is listening on {}:{}".format(ownIP, ownPort))
+    pcap_process = multiprocessing.Process(target=write_pcap, args=(measurementID, interface))
+    pcap_process.start()
     sendstart = threading.Event()
     killevent = threading.Event()
     killevent.set()
@@ -100,3 +134,5 @@ if __name__ == "__main__":
             print("keyboardinterrupt detected")
             sendstart.set()
             killevent.clear()
+            time.sleep(3)
+            pcap_process.terminate()
