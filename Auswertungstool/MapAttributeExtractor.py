65551a8447c7d2513eb0e1533a791fc54322f70c
Alexander <aditt@uos.de>
2018-12-13 20:10:42 +0100

    fully functional extractor for the map attributes

--- Auswertungstool/MapAttributeExtractor.py
+++ Auswertungstool/MapAttributeExtractor.py
@@ -1,5 +1,8 @@
-import sys, getopt, os, math, datetime, time
+# Merge the signalstrength and the gps by the timestamp. 
+# To each gps-coordinate the nearest timestamp of a signal within a time 
+# interval of 10 seconds is looked up.  
 
+import sys, getopt, os, math, datetime, time
 
 # convert the format of the gps receiver to decimaldegree
 def convertToDecimal(value):
@@ -34,11 +37,12 @@ if __name__ == "__main__":
         print("Parameters: -g <gps_filepath> -s <signalstrength_filepath>")
         sys.exit()
 
-    print(signalstrength_path)
-    print(gps_path)
+    #concat output filename
+    parts = gps_path.split('/')[1].split('_')
+    origin = parts[0] + "_" + parts[1] + "_" + parts[2] + "_" + parts[3] + "_MapAttributes.txt"
 
-    result_file = open("coordinates.txt", "w") 
-    result_file.write("Timestamp_GPS, Latitude, Longtitude, Signalstrength, Diff_TS_GPS_SS \n")
+    result_file = open(origin, "w") 
+    result_file.write("Timestamp_GPS, Cellid, Latitude, Longtitude, Signalstrength, Diff_TS_GPS_SS \n")
     gps_file=open(gps_path,'r')
     print(gps_file.name)
     ts_dmy = ""
@@ -53,19 +57,19 @@ if __name__ == "__main__":
             lon_rad = convertToDecimal(float(items[4]))
             ts = datetime.datetime.strptime((ts_dmy + " " + ts_hms), '%d%m%y %H%M%S')
             ts_unix = time.mktime(ts.timetuple()) + (float(ms) / 1000) + 3600 # plus 3600 (= 60 sec * 60 min) because different time offset 
-            # find nearest signalstrength to textfile
+            # find nearest signalstrength to textfile, within 10 seconds timediff
             signalstrength_file = open(signalstrength_path,'r')
-            min_ts_diff = 1000.0 # in seconds
+            min_ts_diff = 10.0 # in seconds
+            cellid = 0
             for line in signalstrength_file:
-                items = line.split(';') # timestamp , technology, dont know, signalstrength, dont know...
+                items = line.split(';') # timestamp , technology, cellid, dont know, signalstrength, dont know...
                 if items[1] == "LTE":
                     ts_diff = abs(float(items[0]) - ts_unix)
                     if ts_diff < min_ts_diff:
-                        signalstrength = items[3].split("dbm")[0]
+                        signalstrength = items[4].split("d")[0]
                         min_ts_diff = ts_diff
-                    else:
-                        break                        
-            result_file.write(ts_hms + "," + str(lat_rad) + "," + str(lon_rad) + "," + str(signalstrength) + "," + str(min_ts_diff) + "\n")
+                        cellid = int(items[2])                       
+            result_file.write(str(ts_unix) + "," + str(cellid) + "," + str(lat_rad) + "," + str(lon_rad) + "," + str(signalstrength) + "," + str(min_ts_diff) + "\n")
                
     gps_file.close()
     result_file.close()
