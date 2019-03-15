6869a6e7cadb4fb85a26674db575526345f8137b
Dominic <dlaniewski@uos.de>
2019-03-15 18:23:07 +0100

    aufgeräumt

--- Archiv/Old_Messtool_CPP/Auswertung/R-Script.R
+++
@@ -1,9 +0,0 @@
-bandbreite <- read.table("Documents/Prototyp/Messtool/Server/build/bandwidths.txt")
-x <- seq(1,5000)
-plot(x, bandbreite[,1], ylim=c(10000, 200000))
-abline(h=36379, lty=1, lwd=2)
-abline(h=51379, lty=3, lwd=2)
-abline(h=21379, lty=3, lwd=2)
-abline(h=35985, lty=1, col="red") #Median des Epsilonbereichs
-abline(h=41204, lty=1, col="blue") # Gesamtmedian
-median(bandbreite[,1])
